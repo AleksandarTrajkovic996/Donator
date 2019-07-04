b965fcff551cfb5bea8d6c6bea6467faf1c1c001
rasicmihailo <rasicmihailo@elfak.rs>
2019-07-04 04:03:45 +0200

    poz

--- Donator2/app/src/main/java/com/arteam/donator/MainActivity.java
+++ Donator2/app/src/main/java/com/arteam/donator/MainActivity.java
@@ -9,7 +9,7 @@ import com.google.firebase.auth.FirebaseUser;
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseUser user;
-
+int a;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
