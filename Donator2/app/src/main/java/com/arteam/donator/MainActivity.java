f04c783a751475c1f3222063f14d493ab261846f
rasicmihailo <rasicmihailo@elfak.rs>
2019-07-04 04:03:08 +0200

    poz

---
+++ Donator2/app/src/main/java/com/arteam/donator/MainActivity.java
@@ -0,0 +1,20 @@
+package com.arteam.donator;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseUser user;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
