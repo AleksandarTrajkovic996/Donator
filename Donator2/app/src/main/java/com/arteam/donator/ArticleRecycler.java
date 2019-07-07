396b9027ee3c90b4905618ca96d26d1418633ef5
rasicmihailo <rasicmihailo@elfak.rs>
2019-07-07 02:43:52 +0200

    donate

---
+++ Donator2/app/src/main/java/com/arteam/donator/ArticleRecycler.java
@@ -0,0 +1,231 @@
+package com.arteam.donator;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArticleRecycler extends RecyclerView.Adapter<ArticleRecycler.ViewHolder> {
+
+    private Context context;
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore firebaseFirestore;
+
+
+    private Map<Integer,Article> list;
+
+    private RelativeLayout relativeLayout;
+    private TextView txtName;
+    private TextView txtSize;
+    private TextView txtDescription;
+    private Button btnOk;
+    private Button btnOk2;
+    private Button btnCancel;
+    private FloatingActionButton fab;
+    private LinearLayout linearLayout3;
+    private LinearLayout linearLayout2;
+    private boolean relLayoutActive;
+
+
+    public ArticleRecycler(Map<Integer, Article> listArticles, RelativeLayout relAddArticle, TextView txtName, TextView txtSize, TextView txtDescription, Button btnOk, Button btnOk2, Button btnCancel, FloatingActionButton fab, LinearLayout lin2, LinearLayout lin3) {
+        this.list = listArticles;
+        this.relativeLayout = relAddArticle;
+        this.txtName = txtName;
+        this.txtSize = txtSize;
+        this.txtDescription = txtDescription;
+        this.btnOk = btnOk;
+        this.btnOk2 = btnOk2;
+        this.btnCancel = btnCancel;
+        this.relLayoutActive = false;
+        this.linearLayout3 = lin3;
+        this.linearLayout2 = lin2;
+        this.fab = fab;
+    }
+
+    @NonNull
+    @Override
+    public ArticleRecycler.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.one_data, viewGroup, false);
+        context = viewGroup.getContext();
+
+        mAuth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ArticleRecycler.ViewHolder holder, int position) {
+
+        final String articleId = list.get(position).articleID;
+
+        final String name = list.get(position).getName();
+
+        holder.setTxtDisplay(name);
+
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(!relLayoutActive){
+                    txtName.setText("");
+                    txtSize.setText("");
+                    txtDescription.setText("");
+                    relativeLayout.setVisibility(View.VISIBLE);
+                    linearLayout2.setVisibility(View.VISIBLE);
+                    linearLayout3.setVisibility(View.INVISIBLE);
+                    relLayoutActive = true;
+                }
+
+
+            }
+        });
+
+        final Map<String, String> articleForAdd = new HashMap<>();
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                articleForAdd.put("name", txtName.getText().toString()); //vlasnikov id
+                articleForAdd.put("size", txtSize.getText().toString());
+                articleForAdd.put("description", txtDescription.getText().toString());
+                articleForAdd.put("type", "donate");
+
+
+                firebaseFirestore.collection("Users/" + mAuth.getCurrentUser().getUid() + "/Articles").add(articleForAdd)
+                        .addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+                            @Override
+                            public void onComplete(@NonNull Task<DocumentReference> task) {
+
+                                if(task.isSuccessful()){
+                                    //holder.setTxtPrikaz("proslo");
+
+                                }else{
+
+                                }
+
+                            }
+                        });
+
+                relativeLayout.setVisibility(View.INVISIBLE);
+                linearLayout2.setVisibility(View.INVISIBLE);
+                linearLayout3.setVisibility(View.INVISIBLE);
+                relLayoutActive=false;
+            }
+        });
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                relativeLayout.setVisibility(View.INVISIBLE);
+                linearLayout2.setVisibility(View.INVISIBLE);
+                linearLayout3.setVisibility(View.INVISIBLE);
+                relLayoutActive=false;
+            }
+        });
+
+        holder.one_data.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!relLayoutActive){
+
+                    firebaseFirestore.collection("Users/" + mAuth.getCurrentUser().getUid() + "/Articles").document(articleId).get()
+                            .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                @Override
+                                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+
+                                    if (task.isSuccessful()) {
+
+                                        if (task.getResult().exists()) {
+
+                                            Article article = task.getResult().toObject(Article.class);
+
+                                            txtName.setText(article.getName());
+                                            txtSize.setText(article.getSize());
+                                            txtDescription.setText(article.getDescription());
+
+                                            relativeLayout.setVisibility(View.VISIBLE);
+                                            linearLayout2.setVisibility(View.INVISIBLE);
+                                            linearLayout3.setVisibility(View.VISIBLE);
+                                            relLayoutActive = true;
+                                        } else {
+
+                                        }
+
+                                    } else {
+
+                                    }
+
+
+                                }
+                            });
+
+
+
+                    btnOk2.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            relativeLayout.setVisibility(View.INVISIBLE);
+                            linearLayout2.setVisibility(View.INVISIBLE);
+                            linearLayout3.setVisibility(View.INVISIBLE);
+                            relLayoutActive=false;
+                        }
+                    });
+
+                }
+            }
+        });
+
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+
+        private TextView txtDisplay;
+        private View view;
+        private TextView one_data;
+
+
+        @SuppressLint("ResourceType")
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            view = itemView;
+            one_data = (TextView) view.findViewById(R.id.txtDisplay);
+        }
+
+
+        public void setTxtDisplay(String txt){
+
+            txtDisplay = view.findViewById(R.id.txtDisplay);
+
+            txtDisplay.setText(txt);
+        }
+    }
+}
--- Donator2/app/src/main/java/com/arteam/donator/DonateFragment.java
+++ Donator2/app/src/main/java/com/arteam/donator/DonateFragment.java
@@ -4,29 +4,110 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class DonateFragment extends Fragment {
 
     View view;
 
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore firebaseFirestore;
+    private RecyclerView recyclerView;
+    private Map<Integer, Article> listArticles;
+    private ArticleRecycler articleRecycler;
+    private RelativeLayout relAddArticle;
+    private LinearLayout linearLayout2;
+    private LinearLayout linearLayout3;
+    private Button btnOk;
+    private Button btnOk2;
+    private Button btnCancel;
+    private TextView txtName;
+    private TextView txtSize;
+    private TextView txtDescription;
+    FloatingActionButton fab;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_donate, container, false);
 
-        FloatingActionButton fab = view.findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        relAddArticle = view.findViewById(R.id.relAddArticle);
+        btnOk = view.findViewById(R.id.allow);
+        btnOk2 = view.findViewById(R.id.allow2);
+        btnCancel = view.findViewById(R.id.deny);
+        txtName = view.findViewById(R.id.txtName);
+        txtSize = view.findViewById(R.id.txtSize);
+        txtDescription = view.findViewById(R.id.txtDesc);
+        linearLayout3 = view.findViewById(R.id.lin3);
+        linearLayout2 = view.findViewById(R.id.lin2);
+        fab = view.findViewById(R.id.fab);
+
+
+
+
+
+
+        recyclerView = view.findViewById(R.id.listArticleRecycler);
+
+
+        listArticles = new HashMap<>();
+        mAuth = FirebaseAuth.getInstance();
+        firebaseFirestore= FirebaseFirestore.getInstance();
+
+
+
+        articleRecycler = new ArticleRecycler(listArticles, relAddArticle, txtName, txtSize, txtDescription, btnOk, btnOk2, btnCancel, fab, linearLayout2, linearLayout3);
+
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(container.getContext()));
+        recyclerView.setAdapter(articleRecycler);
+
+
+        firebaseFirestore.collection("Users/" + mAuth.getCurrentUser().getUid() + "/Articles")
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@javax.annotation.Nullable QuerySnapshot queryDocumentSnapshots, @javax.annotation.Nullable FirebaseFirestoreException e) {
+                        int i = 0;
+                        for (DocumentChange doc : queryDocumentSnapshots.getDocumentChanges()) {
+                            if (doc.getType() == DocumentChange.Type.ADDED) {
+                                //       Article konkurs = doc.getDocument().toObject(Article.class);
+                                //       listArticles.add(konkurs);
+
+                                //       vlasnikKonkursRecycler.notifyDataSetChanged();
+                                //       String tip = doc.getDocument().getString("tip");
+                                //      String zaposljen = doc.getDocument().getString("zaposljen");
+                               // if (tip.matches("radnik") && zaposljen.matches("ne")) {
+
+                                    String articleID = doc.getDocument().getId();
+                                    Article article = doc.getDocument().toObject(Article.class).withId(articleID, i);
+                                    listArticles.put(i, article);
+                                    i++;
+                                    articleRecycler.notifyDataSetChanged();
+
+                               // }
+                            }
+                        }
+                    }
+                });
+
 
 
         return view;
